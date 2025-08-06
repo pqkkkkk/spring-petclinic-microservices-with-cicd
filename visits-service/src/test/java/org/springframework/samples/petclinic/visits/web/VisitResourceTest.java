0884c66f412e02f25f88a578e5856b097a20c751
pqkkkk <22120174@student.hcmus.edu.vn>
2025-08-06 11:12:49 +0700

    visit service: add a test case for create visit endpoint

--- visits-service/src/test/java/org/springframework/samples/petclinic/visits/web/VisitResourceTest.java
+++ visits-service/src/test/java/org/springframework/samples/petclinic/visits/web/VisitResourceTest.java
@@ -5,18 +5,27 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.samples.petclinic.visits.model.Visit;
 import org.springframework.samples.petclinic.visits.model.VisitRepository;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.assertj.MockMvcTester.MockMvcRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import static java.util.Arrays.asList;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.Date;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(VisitResource.class)
@@ -26,7 +35,7 @@ class VisitResourceTest {
     @Autowired
     MockMvc mvc;
 
-    @MockBean
+    @MockitoBean
     VisitRepository visitRepository;
 
     @Test
@@ -57,5 +66,26 @@ class VisitResourceTest {
             .andExpect(jsonPath("$.items[0].petId").value(111))
             .andExpect(jsonPath("$.items[1].petId").value(222))
             .andExpect(jsonPath("$.items[2].petId").value(222));
+    }
+
+    @Test
+    public void createVisit_WithValidData_ShouldReturnCreatedStatus() throws Exception{
+        // Given
+        Visit visit = Visit.VisitBuilder.aVisit()
+            .id(1)
+            .petId(111)
+            .date(new Date())
+            .description("Checkup")
+            .build();
+        
+        when(visitRepository.save(visit)).thenReturn(visit);
+
+        // When & Then
+        mvc.perform(MockMvcRequestBuilders.post("/owners/1/pets/111/visits")
+        .content(new ObjectMapper().writeValueAsString(visit))
+        .contentType(MediaType.APPLICATION_JSON)
+        .accept(MediaType.APPLICATION_JSON)
+        )
+        .andExpect(MockMvcResultMatchers.status().isCreated());
     }
 }
